435aa64231074f0478394a1a2f1aa5d9933445c1
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2026-04-23 06:08:55 +0200

    Remove the Therapeutic Areas section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Footer from "@/components/layout/Footer";
 import InNumbersSection from "@/components/sections/InNumbersSection";
 import CreativeServicesSection from "@/components/sections/CreativeServicesSection";
 import EducationalProgramsSection from "@/components/sections/EducationalProgramsSection";
-import TherapeuticAreasSection from "@/components/sections/TherapeuticAreasSection";
 import CTASection from "@/components/sections/CTASection";
 import CompaniesSection from "@/components/sections/CompaniesSection";
 
@@ -16,7 +15,6 @@ export default function HomePage() {
         <InNumbersSection />
         <CreativeServicesSection />
         <EducationalProgramsSection />
-        <TherapeuticAreasSection />
         <CTASection />
       </main>
       <Footer />
